944987cc68edd776e47899764537e3a556f34eb2
Mahmoud <mahmoudabdelkhalik131@gmail.com>
2025-04-24 04:04:02 +0200

    just trying a new thing

--- src/Project/project.routes.ts
+++ src/Project/project.routes.ts
@@ -9,4 +9,4 @@ projectRouter.route('/:id')
 .get(projectValidation.getone,projectServices.getOne)
 .put(projectValidation.updateOne,projectServices.updateOne)
 .delete(projectValidation.deleteOne,projectServices.deleteOne)
-export default projectRouter
+export default projectRouter;

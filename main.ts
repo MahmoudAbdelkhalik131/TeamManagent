cc4448670d1789b8cb5c8a659795e43b488c392c
Mahmoud <mahmoudabdelkhalik131@gmail.com>
2025-10-28 05:44:47 +0300

    Adding helmet

--- main.ts
+++ main.ts
@@ -4,10 +4,12 @@ import Connection from "./config";
 import Routes from "./src";
 import RateLimiter from "./src/middlewares/ratelimiter";
 import cors from "cors"
+import helmet  from "helmet";
 const app: express.Application = express();
 app.use(express.json({limit:'1mb'}));
 dotenv.config();
 Connection();
+app.use(helmet())
 app.use(cors());
 app.use( RateLimiter)
 app.listen(process.env.PORT, () => {

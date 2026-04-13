48b81c43d41f0fb48b2d18a2aa93e122b144be51
Mahmoud <mahmoudabdelkhalik131@gmail.com>
2026-04-13 16:32:04 +0200

    smtp

--- src/utils/sendEmail.ts
+++ src/utils/sendEmail.ts
@@ -7,8 +7,8 @@ interface options {
 const sendEmail = async (options: options): Promise<void> => {
   const transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
-    port: 465,
-    secure: true,
+    port: 587,
+    secure: false,
     auth: {
       user: process.env.EMAIL_USERNAME,
       pass: process.env.EMAIL_PASSWORD,

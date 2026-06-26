dc61772f7af45a0573a98aa38a989d6c2053e7da
Mahmoud <mahmoudabdelkhalik131@gmail.com>
2026-06-27 01:41:19 +0300

    deleting buffer

--- config.ts
+++ config.ts
@@ -2,9 +2,6 @@ import mongoose from "mongoose";
 
 const Connection = async () => {
    try {
-    // Disable buffering so we get immediate errors if connection fails
-    mongoose.set('bufferCommands', false); 
-    
     await mongoose.connect(process.env.Mongo!);
     console.log(`Connected to Database: ${mongoose.connection.host}`);
   } catch (e) {
